20b4812276e46a6cc5de95704427245778af5548
Tom Voet <77166831+tomvoet@users.noreply.github.com>
2024-04-17 18:20:31 +0200

    feat(rqlite-rs): add https support

---
+++ examples/https.rs
@@ -0,0 +1,28 @@
+use rqlite_rs::{config::Scheme, prelude::*};
+
+#[derive(FromRow)]
+pub struct Table {
+    name: String,
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let client = RqliteClientBuilder::new()
+        .known_host("localhost:4001")
+        .scheme(Scheme::Https)
+        .build()?;
+
+    let query = rqlite_rs::query!(
+        "SELECT name FROM sqlite_master WHERE type='table' AND name NOT LIKE 'sqlite_%'"
+    )?;
+
+    let rows = client.fetch(query).await?;
+
+    let tables = rows.into_typed::<Table>()?;
+
+    for table in tables {
+        println!("Table: {}", table.name);
+    }
+
+    Ok(())
+}
