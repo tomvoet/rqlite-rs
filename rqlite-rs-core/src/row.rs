035f5e7dcbba9aefbe86e47dd1ee02d0e9b9945a
Tom Voet <tomvoet@gmx.de>
2024-04-23 00:49:49 +0200

    test(rqlite-rs-core): added tests to Row

--- rqlite-rs-core/src/row.rs
+++ rqlite-rs-core/src/row.rs
@@ -123,3 +123,152 @@ impl Row {
         T::from_row(self)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn create_row() -> Row {
+        use serde_json::json;
+
+        let columns = Arc::new(vec![
+            Column::new("id".to_string(), 0usize, "integer".to_string()),
+            Column::new("name".to_string(), 1usize, "text".to_string()),
+        ]);
+
+        let column_names = Arc::new(
+            columns
+                .iter()
+                .enumerate()
+                .map(|(i, c)| (c.name().to_string(), i))
+                .collect::<HashMap<String, usize>>(),
+        );
+
+        let values = vec![json!(1), json!("test")].into_boxed_slice();
+
+        Row::new(&columns, &column_names, values)
+    }
+
+    #[test]
+    fn unit_row_get() {
+        let row = create_row();
+
+        assert_eq!(row.get::<i32>("id").unwrap(), 1);
+        assert_eq!(row.get::<String>("name").unwrap(), "test");
+        assert!(matches!(
+            row.get::<i32>("not_found").unwrap_err(),
+            IntoTypedError::ColumnNotFound
+        ));
+    }
+
+    #[test]
+    fn unit_row_get_opt() {
+        let row = create_row();
+
+        assert_eq!(row.get_opt::<i32>("id").unwrap(), Some(1));
+        assert_eq!(
+            row.get_opt::<String>("name").unwrap(),
+            Some("test".to_string())
+        );
+        assert_eq!(row.get_opt::<i32>("not_found").unwrap(), None);
+    }
+
+    #[test]
+    fn unit_row_get_by_index() {
+        let row = create_row();
+
+        assert_eq!(row.get_by_index::<i32>(0).unwrap(), 1);
+        assert_eq!(row.get_by_index::<String>(1).unwrap(), "test");
+        assert!(matches!(
+            row.get_by_index::<i32>(2).unwrap_err(),
+            IntoTypedError::ValueNotFound
+        ));
+    }
+
+    #[test]
+    fn unit_row_get_by_index_opt() {
+        let row = create_row();
+
+        assert_eq!(row.get_by_index_opt::<i32>(0).unwrap(), Some(1));
+        assert_eq!(
+            row.get_by_index_opt::<String>(1).unwrap(),
+            Some("test".to_string())
+        );
+        assert_eq!(row.get_by_index_opt::<i32>(2).unwrap(), None);
+    }
+
+    #[test]
+    fn unit_row_columns() {
+        let row = create_row();
+
+        assert_eq!(row.columns().len(), 2);
+        assert_eq!(row.columns()[0].name(), "id");
+        assert_eq!(row.columns()[1].name(), "name");
+    }
+
+    #[test]
+    fn unit_row_column_names() {
+        let row = create_row();
+
+        assert_eq!(row.column_names().len(), 2);
+        assert_eq!(row.column_names()["id"], 0);
+        assert_eq!(row.column_names()["name"], 1);
+    }
+
+    #[test]
+    fn unit_row_values() {
+        let row = create_row();
+
+        assert_eq!(row.values().len(), 2);
+        assert_eq!(row.values()[0], serde_json::json!(1));
+        assert_eq!(row.values()[1], serde_json::json!("test"));
+    }
+
+    #[test]
+    fn unit_row_len() {
+        let row = create_row();
+
+        assert_eq!(row.len(), 2);
+    }
+
+    #[test]
+    fn unit_row_is_empty() {
+        let row = create_row();
+
+        assert!(!row.is_empty());
+    }
+
+    #[test]
+    fn unit_row_into_typed_tuple() {
+        let row = create_row();
+
+        let (id, name) = row.into_typed::<(i32, String)>().unwrap();
+
+        assert_eq!(id, 1);
+        assert_eq!(name, "test");
+    }
+
+    #[test]
+    fn unit_row_into_typed_struct() {
+        struct Test {
+            id: i32,
+            name: String,
+        }
+
+        impl FromRow for Test {
+            fn from_row(row: Row) -> Result<Self, IntoTypedError> {
+                Ok(Test {
+                    id: row.get("id")?,
+                    name: row.get("name")?,
+                })
+            }
+        }
+
+        let row = create_row();
+
+        let test = row.into_typed::<Test>().unwrap();
+
+        assert_eq!(test.id, 1);
+        assert_eq!(test.name, "test");
+    }
+}
